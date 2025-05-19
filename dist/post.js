ff42fd6ae62d413af3c34f2994ffefe8c0b68002
pberton <1789738+pberton@users.noreply.github.com>
2025-05-19 10:26:30 -0700

    transpile

---
+++ dist/post.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * The entrypoint for the action.
+ */
+const main_1 = require("./main");
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(0, main_1.logout)();
+//# sourceMappingURL=post.js.map
